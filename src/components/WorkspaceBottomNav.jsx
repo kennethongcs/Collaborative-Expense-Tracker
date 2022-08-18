616c8e0ec574bf693fc5bd51f4c74e34b3ee23d2
Hertanto Irawan <hertanto.irawan@outlook.com>
2022-08-19 00:59:07 +0700

    feat(init): workspace form and layout

--- src/components/WorkspaceBottomNav.jsx
+++ src/components/WorkspaceBottomNav.jsx
@@ -2,12 +2,28 @@ import React, { useState } from 'react';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import { NavLink } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
+import MobileStepper from '@mui/material/MobileStepper';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
 const WorkspaceBottomNav = () => {
   const [value, setValue] = useState('recents');
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+  };
+
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
   return (
@@ -52,6 +68,33 @@ const WorkspaceBottomNav = () => {
       >
         4
       </NavLink>
+      <MobileStepper
+        variant="dots"
+        steps={4}
+        position="static"
+        activeStep={activeStep}
+        sx={{ maxWidth: 400, flexGrow: 1 }}
+        nextButton={(
+          <Button size="small" onClick={handleNext} disabled={activeStep === 3}>
+            Next
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+      )}
+        backButton={(
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+      )}
+      />
     </>
   );
 };
--- src/pages/WorkspaceForm.jsx
+++ src/pages/WorkspaceForm.jsx
@@ -1,10 +1,116 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios'; import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 
-const WorkspaceForm = () => (
-  <div>
-    <div>Create new workspace</div>
-    <div>Ask for name and purpose</div>
-  </div>
-);
+const WorkspaceForm = () => {
+  const [workspaceName, setWorkspaceName] = useState('');
+  const [purpose, setPurpose] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (workspaceName) {
+      console.log('email exists!!');
+      // axios
+      //   .post('/login', {
+      //     email,
+      //     password,
+      //   })
+      //   .then((response) => {
+      //     console.log(response.data);
+
+      // TODO: get workspace info from workspace api
+      const workspaceId = null;
+
+      // setWorkspace(workspaceId);
+
+      navigate('/workspace/2');
+      // })
+      // .catch((error) => console.log(error));
+    } else {
+      console.log('nothing entered');
+    }
+  };
+
+  return (
+    <Grid container component="main" sx={{ height: '100vh' }}>
+      <CssBaseline />
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: 'url(https://source.unsplash.com/random)',
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: (t) => (t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900]),
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="workspaceName"
+              label="Workspace Name"
+              name="workspaceName"
+              autoComplete="workspaceName"
+              autoFocus
+              onChange={(event) => {
+                setWorkspaceName(event.target.value);
+              }}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              multiline
+              rows={4}
+              name="purpose"
+              label="Purpose"
+              id="purpose"
+              autoComplete="purpose"
+              onChange={(event) => {
+                setPurpose(event.target.value);
+              }}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Next
+            </Button>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default WorkspaceForm;
