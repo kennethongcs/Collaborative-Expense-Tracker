0581fdd9ba77df41547525723709a1cce74ad452
kennethongcs <kenneth.ongcs@gmail.com>
2022-08-19 21:43:17 +0800

    add buttons and functions to get data from input

--- src/pages/CollaboratorForm.jsx
+++ src/pages/CollaboratorForm.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import AppBar from '../components/AppBar.jsx';
 
-const CollaboratorForm = () => (
-  <>
-    <AppBar />
-    <div>Add collaborator</div>
-    <div>Probably similar to adding categories but with collaborators</div>
-  </>
-);
+const CollaboratorForm = () => {
+  const [collaborator, setCollaborator] = useState('');
+  const collaboratorName = useRef();
+
+  const getCollaboratorName = () => {
+    // should set timeout on this to prevent many queries to server
+    const input = collaboratorName.current.value;
+    console.log(input);
+  };
+  const submitEmail = () => {
+    const input = collaboratorName.current.value;
+    console.log(input);
+  };
+  return (
+    <div>
+      <div>Add collaborator page</div>
+      <input
+        type="text"
+        placeholder="collaborators email / username"
+        ref={collaboratorName}
+        onChange={getCollaboratorName}
+      ></input>
+      <button onClick={submitEmail}>Collaborate!</button>
+      {/* upon input, query db for users with that email / username */}
+    </div>
+  );
+};
 
 export default CollaboratorForm;
