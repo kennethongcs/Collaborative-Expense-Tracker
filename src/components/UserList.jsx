6455b622113e3f20b0de1d566d88afaf6eb68794
kennethongcs <kenneth.ongcs@gmail.com>
2022-08-22 23:11:14 +0800

    feat: logic to loop through email array from db

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -3,15 +3,21 @@ import React from 'react';
 const IndividualUser = ({ user, whenUserIsClicked }) => {
   const onUserClick = () => {
     whenUserIsClicked(user);
-    console.log(user.name);
+    console.log(`added: ${user.email}`);
   };
   // TODO add uuid for key in prop
-  return <li onClick={onUserClick}>{user.name}</li>;
+  return <li onClick={onUserClick}>{user.email}</li>;
 };
 
 export default function UserList({ retrievedUsers, whenUserIsClicked }) {
   return retrievedUsers.map((user) => {
     // onclick to add that users email into input box
-    return <IndividualUser user={user} whenUserIsClicked={whenUserIsClicked} />;
+    return (
+      <IndividualUser
+        key={user.id}
+        user={user}
+        whenUserIsClicked={whenUserIsClicked}
+      />
+    );
   });
 }
