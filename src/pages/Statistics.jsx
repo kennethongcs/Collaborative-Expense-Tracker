6ec1398c1f14a4cfdaf95ab0eb17ba634224fb3c
Hertanto Irawan <hertanto.irawan@outlook.com>
2022-08-23 15:23:11 +0700

    feat: add mui layout to statistics pageholder

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Typography, Box } from '@mui/material';
 
 const Statistics = () => (
-  <div>
-    <div>Statistics</div>
-    <div>A bunch of charts and list maybe</div>
-  </div>
+  <>
+    <Typography component="h1" variant="h5">
+      Statistics
+    </Typography>
+    <Box sx={{ mt: 3 }}>
+      <div>
+        <div>A bunch of charts and list maybe</div>
+      </div>
+    </Box>
+  </>
 );
 
 export default Statistics;
