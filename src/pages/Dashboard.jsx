12d4653ba0921e1fcd26b762635f885ec5fd412b
kennethongcs <kenneth.ongcs@gmail.com>
2022-08-30 16:30:43 +0800

    feat: add getExpenses route

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,40 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import axios from 'axios';
+import ExpenseList from '../components/ExpenseList.jsx';
 
-const Dashboard = ({ user, workspace }) => (
-  <>
-    <Typography component="h1" variant="h5">
-      Dashboard
-    </Typography>
-    <Box sx={{ mt: 3 }}>
-      <div>
-        <div>Menu to change workspace if more than 1</div>
-        <div>Chart for the current workspace</div>
-        <div>Some info about collaborators in this workspace</div>
-        <div>Recent expenses for this workspace</div>
-        <br />
-        <div>Currently looking at workspace {workspace?.name}</div>
-        <br />
+const Dashboard = ({ user, workspace }) => {
+  const [expenses, setExpenses] = useState([]);
+
+  useEffect(() => {
+    axios
+      .post('/getExpenses', {
+        workspace,
+      })
+      .then((res) => {
+        // console.log(res.data);
+        setExpenses(res.data);
+      });
+  }, []);
+  return (
+    <>
+      <Typography component="h1" variant="h5">
+        Chart here
+      </Typography>
+      <Box sx={{ mt: 3 }}>
         <div>
-          Today
+          <div>Menu to change workspace if more than 1</div>
+          <div>Chart for the current workspace</div>
+          <div>Some info about collaborators in this workspace</div>
+          <div>Recent expenses for this workspace</div>
+          <br />
+          <div>Currently looking at workspace {workspace?.name}</div>
+          <ul>
+            <ExpenseList expenses={expenses} />
+          </ul>
           <div>
-            <NavLink to="expenses/123">Nike Store (clothing)</NavLink>
-            -$100
+            Today
+            <div>
+              <NavLink to="expenses/123">Nike Store (clothing)</NavLink>
+              -$100
+            </div>
+          </div>
+          <br />
+          <div>
+            31 July 2022
+            <div>
+              <NavLink to="expenses/789">Apple Store (electronics)</NavLink>
+              -$500
+            </div>
           </div>
         </div>
-        <br />
-        <div>
-          31 July 2022
-          <div>
-            <NavLink to="expenses/789">Apple Store (electronics)</NavLink>
-            -$500
-          </div>
-        </div>
-      </div>
-    </Box>
-  </>
-);
+      </Box>
+    </>
+  );
+};
 
 export default Dashboard;
