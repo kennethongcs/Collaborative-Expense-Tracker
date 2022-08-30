0d8b0b9b32c31c43adc6775241607d5f6740bb72
kennethongcs <kenneth.ongcs@gmail.com>
2022-08-30 16:28:47 +0800

    feat: expenses controller

--- routes.mjs
+++ routes.mjs
@@ -3,11 +3,13 @@ import db from './models/index.mjs';
 import initUsersController from './controllers/users-controller.mjs';
 import initWorkspacesController from './controllers/workspaces-controller.mjs';
 import initCategoriesController from './controllers/categories-controller.mjs';
+import initExpensesController from './controllers/expenses-controller.mjs';
 
 export default function routes(app) {
   const usersController = initUsersController(db);
   const workspacesController = initWorkspacesController(db);
   const categoriesController = initCategoriesController(db);
+  const expensesController = initExpensesController(db);
 
   app.get('/signup', usersController.signup);
   app.post('/signup', usersController.signup);
@@ -23,6 +25,8 @@ export default function routes(app) {
 
   app.post('/workspace', workspacesController.create);
   app.post('/joinworkspace', workspacesController.joinWorkspace);
+
+  app.post('/getexpenses', expensesController.retrieve);
 
   app.get('*', (request, response) => {
     response.sendFile(resolve('dist', 'main.html'));
