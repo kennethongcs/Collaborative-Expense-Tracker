8796317bf69568af92be8d905f950aaddd212fae
Hertanto Irawan <hertanto.irawan@outlook.com>
2022-09-01 01:45:50 +0700

    fix: handle bottom nav select on refresh

--- src/components/ExpensesPieChart.jsx
+++ src/components/ExpensesPieChart.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { PieChart, Pie, ResponsiveContainer } from 'recharts';
+import {
+  PieChart, Pie, ResponsiveContainer, Cell, Legend, Tooltip,
+} from 'recharts';
 import axios from 'axios';
 import useTheme from '@mui/material/styles/useTheme';
 
@@ -7,6 +9,8 @@ const ExpensesPieChart = ({ workspace }) => {
   const [pieChartData, setPieChartData] = useState(null);
 
   const colors = useTheme().palette;
+  // eslint-disable-next-line max-len
+  const chartColors = [colors.primary.main, colors.secondary.main, colors.error.main, colors.warning.main, colors.info.main, colors.success.main, colors.primary.light, colors.secondary.light, colors.error.light, colors.warning.light, colors.info.light, colors.success.light, colors.primary.dark, colors.secondary.dark, colors.error.dark, colors.warning.dark, colors.info.dark, colors.success.main];
 
   useEffect(() => {
     axios
@@ -22,7 +26,9 @@ const ExpensesPieChart = ({ workspace }) => {
     <>
       <ResponsiveContainer width="100%" height={300}>
         <PieChart>
-          <Pie data={pieChartData} dataKey="amount" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill={colors.primary.main} label />
+          <Pie data={pieChartData} dataKey="amount" cx="50%" cy="50%" innerRadius={70} outerRadius={90} paddingAngle={5} fill={colors.primary.main} label />
+          <Legend />
+          <Tooltip />
         </PieChart>
       </ResponsiveContainer>
     </>
