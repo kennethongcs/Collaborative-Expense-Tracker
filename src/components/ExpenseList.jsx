8b8f48d50eca700d5c089e33146afc79633e42fd
kennethongcs <kenneth.ongcs@gmail.com>
2022-09-01 00:00:10 +0800

    removed groupBy (not working yet)

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -6,33 +6,37 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
 const expenseList = ({ expenses }) => {
+  // TODO - group items with similar dates together
   const groupedResults = groupBy(
     expenses,
     (result) =>
       // moment(result['Date'], 'DD/MM/YYYY').startOf('isoWeek')
-      moment(result['Date']),
+      moment(result),
     'DD/MM/YYYY'
   );
-  // console.log(groupedResults);
-  return expenses.map((expense) => (
-    <Container key={expense.id}>
-      <Box>
-        {moment().format('DD MMM YYYY', expense.createdAt)}
-        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Box>{expense.payee.name}</Box>
-          <Typography style={{ color: 'red' }} component="span">
-            <Box>{expense.amount}</Box>
-          </Typography>
-        </Box>
-        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <Typography style={{ color: 'grey' }}>
-            {expense.category.name}
-          </Typography>
-        </Box>
-      </Box>
-      <hr />
-    </Container>
-  ));
+  return expenses.map(
+    (expense, index) =>
+      // limit number of expenses to show to 3
+      index < 3 && (
+        <Container key={expense.id}>
+          <Box>
+            {moment().format('DD MMM YYYY', expense.createdAt)}
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Box>{expense.payee.name}</Box>
+              <Typography style={{ color: 'red' }} component="span">
+                <Box>{expense.amount}</Box>
+              </Typography>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Typography style={{ color: 'grey' }}>
+                {expense.category.name}
+              </Typography>
+            </Box>
+          </Box>
+          <hr />
+        </Container>
+      )
+  );
 };
 
 export default expenseList;
