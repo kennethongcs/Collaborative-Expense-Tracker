1d6501de81236e2297f07c630da9cc2bcca47b83
kennethongcs <kenneth.ongcs@gmail.com>
2022-09-01 15:47:08 +0800

    feat: add show as prop helps to show different number of expenses

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -7,54 +7,58 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 
-const MapOfExpenses = ({ value }) =>
-  value.map((expense) => (
-    <Container key={expense.id}>
-      <Box mt={1}>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Box>{expense.payee.name}</Box>
-          <Typography style={{ color: 'red' }} component="span">
-            <Box>{expense.amount}</Box>
-          </Typography>
-        </Box>
-        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <Typography style={{ color: 'grey' }}>
-            {expense.category.name}
-          </Typography>
-        </Box>
-      </Box>
-      <hr />
-    </Container>
-  ));
+const MapOfExpenses = ({ value, show }) =>
+  value.map(
+    (expense, index) =>
+      index < show && (
+        <Container key={expense.id}>
+          <Box mt={1}>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Box>{expense.payee.name}</Box>
+              <Typography style={{ color: 'red' }} component="span">
+                <Box>{expense.amount}</Box>
+              </Typography>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Typography style={{ color: 'grey' }}>
+                {expense.category.name}
+              </Typography>
+            </Box>
+          </Box>
+          <hr />
+        </Container>
+      )
+  );
 
 const ExpenseList = ({ expenses, all }) => {
   const groupedResults = groupBy(expenses, (result) =>
-    // moment(result['Date'], 'DD/MM/YYYY').startOf('isoWeek')
     moment(result).format('DD MMM YYYY')
   );
 
   if (all) {
     for (const [key, value] of Object.entries(groupedResults)) {
+      const show = 50;
       return (
         <>
           <Box mb={1} color="blue">
             {key}
           </Box>
           <Divider color="Lightblue" />
-          <MapOfExpenses value={value} />
+          <MapOfExpenses show={show} value={value} />
         </>
       );
     }
   }
 
   for (const [key, value] of Object.entries(groupedResults)) {
+    const show = 3;
     return (
       <>
         <Box mb={1} color="blue">
           {key}
         </Box>
         <Divider color="Lightblue" />
-        <MapOfExpenses value={value} />
+        <MapOfExpenses show={show} value={value} />
       </>
     );
   }
