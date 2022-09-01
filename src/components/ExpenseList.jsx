1205a4b1c1e3bd34b639317bebc600a836179557
kennethongcs <kenneth.ongcs@gmail.com>
2022-09-01 15:27:33 +0800

    feat: grouped expenses with same dates together

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,66 +1,63 @@
+/* eslint-disable no-restricted-syntax */
 import React from 'react';
 import { groupBy } from 'lodash';
 import moment from 'moment';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+
+const MapOfExpenses = ({ value }) =>
+  value.map((expense) => (
+    <Container key={expense.id}>
+      <Box mt={1}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Box>{expense.payee.name}</Box>
+          <Typography style={{ color: 'red' }} component="span">
+            <Box>{expense.amount}</Box>
+          </Typography>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Typography style={{ color: 'grey' }}>
+            {expense.category.name}
+          </Typography>
+        </Box>
+      </Box>
+      <hr />
+    </Container>
+  ));
 
 const ExpenseList = ({ expenses, all }) => {
-  // TODO - group items with similar dates together
-  const groupedResults = groupBy(
-    expenses,
-    (result) =>
-      // moment(result['Date'], 'DD/MM/YYYY').startOf('isoWeek')
-      moment(result),
-    'DD/MM/YYYY'
+  const groupedResults = groupBy(expenses, (result) =>
+    // moment(result['Date'], 'DD/MM/YYYY').startOf('isoWeek')
+    moment(result).format('DD MMM YYYY')
   );
 
   if (all) {
-    return expenses.map((expense) => (
-      // limit number of expenses to show to 3
-      <Container key={expense.id}>
-        <Box>
-          {moment().format('DD MMM YYYY', expense.createdAt)}
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Box>{expense.payee.name}</Box>
-            <Typography style={{ color: 'red' }} component="span">
-              <Box>{expense.amount}</Box>
-            </Typography>
+    for (const [key, value] of Object.entries(groupedResults)) {
+      return (
+        <>
+          <Box mb={1} color="blue">
+            {key}
           </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Typography style={{ color: 'grey' }}>
-              {expense.category.name}
-            </Typography>
-          </Box>
-        </Box>
-        <hr />
-      </Container>
-    ));
+          <Divider color="Lightblue" />
+          <MapOfExpenses value={value} />
+        </>
+      );
+    }
   }
 
-  return expenses.map(
-    (expense, index) =>
-      // limit number of expenses to show to 3
-      index < 3 && (
-        <Container key={expense.id}>
-          <Box>
-            {moment().format('DD MMM YYYY', expense.createdAt)}
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              <Box>{expense.payee.name}</Box>
-              <Typography style={{ color: 'red' }} component="span">
-                <Box>{expense.amount}</Box>
-              </Typography>
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-              <Typography style={{ color: 'grey' }}>
-                {expense.category.name}
-              </Typography>
-            </Box>
-          </Box>
-          <hr />
-        </Container>
-      )
-  );
+  for (const [key, value] of Object.entries(groupedResults)) {
+    return (
+      <>
+        <Box mb={1} color="blue">
+          {key}
+        </Box>
+        <Divider color="Lightblue" />
+        <MapOfExpenses value={value} />
+      </>
+    );
+  }
 };
 
 export default ExpenseList;
